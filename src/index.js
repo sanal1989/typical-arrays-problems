5dc322bd56180382ce5498f1f9b89cf56430a090
sanal1989 <sanalsalynov@gmail.com>
2020-02-24 01:41:41 +0300

    init: finished task min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof(array) == "undefined"){
+    return 0;
+  }else if(array.length == 0){
+    return 0;
+  }
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof(array) == "undefined"){
+    return 0;
+  }else if(array.length == 0){
+    return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof(array) == "undefined"){
+    return 0;
+  }else if(array.length == 0){
+    return 0;
+  }
+  let sum = 0;
+  for(let i = 0; i<array.length; i++){
+    sum+=array[i];
+  }
+  return sum/array.length;
 }
